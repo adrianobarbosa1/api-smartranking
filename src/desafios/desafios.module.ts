c1a7065b7526bb6ac6bced824b52172d0067bf0f
adrianobarbosa1 <adriano.barbosa100@gmail.com>
2022-04-23 21:38:23 -0300

    add desafio

---
+++ src/desafios/desafios.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DesafiosController } from './desafios.controller';
+import { DesafiosService } from './desafios.service';
+import { DesafioSchema } from './interfaces/desafios.schema';
+
+@Module({
+  imports: [MongooseModule.forFeature([{
+    name: 'Desafio',
+    schema: DesafioSchema
+  }])],
+  controllers: [DesafiosController],
+  providers: [DesafiosService]
+})
+export class DesafiosModule { }
---
+++ src/desafios/interfaces/desafio.interface.ts
@@ -0,0 +1,24 @@
+import { Document } from 'mongoose'
+import { Jogador } from 'src/jogadores/interfaces/jogador.interface';
+
+export interface Desafio extends Document {
+    dataHoraDesafio: Date;
+    status: DesafioStatus;
+    dataHoraSolicitacao: Date;
+    dataHoraResposta: Date;
+    solicitante: Jogador;
+    categoria: string;
+    jogadores: Jogador[];
+    partida: Partida;
+};
+
+export interface Partida extends Document {
+    categoria: string;
+    jogadores: Jogador[];
+    def: Jogador;
+    resultado: Resultado[];
+}
+
+export interface Resultado extends Document {
+    set: string;
+}
