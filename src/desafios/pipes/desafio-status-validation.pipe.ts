d8fa7a22ce0b0b3e158c87abe9f016c863d6d02e
drk <adriano.barbosa100@gmail.com>
2022-04-26 15:42:27 -0300

    att

--- src/desafios/pipes/desafio-status-validation.pipe.ts
+++ src/desafios/pipes/desafio-status-validation.pipe.ts
@@ -2,25 +2,25 @@ import { PipeTransform, BadRequestException } from '@nestjs/common';
 import { DesafioStatus } from '../interfaces/desafio-status.enum';
 
 export class DesafioStatusValidacaoPipe implements PipeTransform {
-  readonly statusPermitidos = [
-    DesafioStatus.ACEITO,
-    DesafioStatus.NEGADO,
-    DesafioStatus.CANCELADO
-  ];
+    readonly statusPermitidos = [
+        DesafioStatus.ACEITO,
+        DesafioStatus.NEGADO,
+        DesafioStatus.CANCELADO,
+    ];
 
-  transform(value: any) {
-    const status = value.status.toUpperCase();
+    transform(value: any) {
+        const status = value.status.toUpperCase();
 
-    if (!this.ehStatusValido(status)) {
-      throw new BadRequestException(`${status} é um status inválido`);
+        if (!this.ehStatusValido(status)) {
+            throw new BadRequestException(`${status} é um status inválido`);
+        }
+
+        return value;
     }
 
-    return value;
-  }
-
-  private ehStatusValido(status: any) {
-    const idx = this.statusPermitidos.indexOf(status);
-    // -1 se o elemento não for encontrado
-    return idx !== -1;
-  }
+    private ehStatusValido(status: any) {
+        const idx = this.statusPermitidos.indexOf(status);
+        // -1 se o elemento não for encontrado
+        return idx !== -1;
+    }
 }
